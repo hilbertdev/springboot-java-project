a1a29674e7d34fc9a7e4477c38165c72b7a3f13c
hilbertmuna <hilbertmuna@gmail.com>
2025-10-01 21:58:05 +0200

    remove unwanted files

--- src/main/java/com/javachatbot/javachatbot/DTOs/ChatCompletionDto.java
+++
@@ -1,6 +0,0 @@
-package com.javachatbot.javachatbot.DTOs;
-
-public class ChatCompletionDto {
-    public String ChatCompletionMessage;
-    public String Choices;
-}
--- src/main/java/com/javachatbot/javachatbot/chatbot/IChatbot.java
+++
@@ -1,7 +0,0 @@
-package com.javachatbot.javachatbot.chatbot;
-
-import com.javachatbot.javachatbot.DTOs.ChatCompletionDto;
-
-public interface IChatbot {
-    ChatCompletionDto CompleteChat(String prompt);
-}
